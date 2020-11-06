92b3210693b69ebae82e22880c70d88745994aae
Vũ Văn Quyền <quyenvv@miichisoft.com>
2020-11-06 15:01:51 +0700

    update paginate

--- app/Repositories/Companies/CompaniesRepository.php
+++ app/Repositories/Companies/CompaniesRepository.php
@@ -13,35 +13,56 @@ class CompaniesRepository implements CompaniesRepositoryInterface
 {
     public function list($data){
         $idSale = Auth::id();
-        $dataCompanies = Companies::whereHas('sales', function ($query)  use ($idSale) {
+        $limit  = 1;
+        $page = 1;
+
+        $dataCompaniesTmp = Companies::whereHas('sales', function ($query)  use ($idSale) {
             $query->where('sale_user_id','=', $idSale);
         })
-        ->with('domains')->get();
+        ->with('domains');
+
+        $recordsTotal = $dataCompaniesTmp->count();
+
+        if(isset($data['size']) && !empty($data['size'])){
+            $limit = $data['size'];
+        }
+        if(isset($data['page']) && !empty($data['page'])){
+            $page = $data['page'];
+        }
+
+        if(isset($data['name']) && !empty($data['name'])){
+            $nameCompaines = $data['name'];
+            $dataCompaniesTmp = $dataCompaniesTmp->where(function ($query) use ($nameCompaines)  {
+                        $query->where('name_jp', 'LIKE', '%'.$nameCompaines.'%')
+                            ->orWhere('name_vn', 'LIKE', '%'.$nameCompaines.'%');
+                    });
+        }
+        if(isset($data['id']) && !empty($data['id'])) {
+            $dataCompaniesTmp = $dataCompaniesTmp->where('id',$data['id']);
+        }
+
+        if(isset($data['domain_id'])) {
+            $arrDomains = array_unique($data['domain_id']);
+             if (array_filter($arrDomains)) {
+                $dataCompaniesTmp = $dataCompaniesTmp->whereHas('domains', function ($query)  use ($arrDomains) {
+                                                    $query->whereIn('domain_id', $arrDomains);
+                });
+            }
+           
+        }
+        $start = ($page - 1) * $limit;
+        $dataCompaniesTmp->skip($start)->take($limit);
+        $dataCompanies = $dataCompaniesTmp->get();
         $dataCompanies->transform(function ($item) {
             $item['domains_show'] = $item['domains']->pluck('label');
             unset($item['domains']);
             return $item;
         });
 
-    return DataTables::of($dataCompanies)
-                    ->filter(function ($query) use ($data) {
-                        if (request()->has('name') && !empty($data['name'])) {
-                            $query->where('name_jp', 'like', "%" . $data['name'] . "%")
-                            ->orWhere('name_vn', 'like', '%' . $data['name'] . '%');
-                        }
-
-                        if (request()->has('id') && !empty($data['id'])) {
-                            $query->where('id', $data['id']);
-                        }
-
-                        if (request()->has('domain_id')) {
-                            $arrDomains = array_unique($data['domain_id']);
-                            if (array_filter($arrDomains)) {
-                                $query->whereHas('domains', function ($query)  use ($arrDomains) {
-                                        $query->whereIn('domain_id', $arrDomains);
-                                    });
-                            }
-                        }
-                    })->make(true);
+        return DataTables::of($dataCompanies)
+                    ->with([
+                        "recordsTotal" => $recordsTotal,
+                    ])
+                    ->make(true);
     }
 }
